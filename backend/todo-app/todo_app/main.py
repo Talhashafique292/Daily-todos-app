2b6f790dc0a4d10e5a222ba31a1c3cfce49a4235
Talha Shafique <talhashafique292@gmail.com>
2024-09-24 16:09:46 +0500

    CRUD functions and contextmanager added

--- backend/todo-app/todo_app/main.py
+++ backend/todo-app/todo_app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-from sqlmodel import SQLModel, Field, create_engine, Session
+from fastapi import FastAPI, Depends, HTTPException
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 from todo_app import setting
-
+from typing import Annotated
+from contextlib import asynccontextmanager
 
 # create model
   # data model is for data validation (pydantic)
@@ -19,47 +20,80 @@ connection_string : str = str(setting.DATABASE_URL).replace("postgresql","postgr
 engine = create_engine(connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
 
 # create tables
-SQLModel.metadata.create_all(engine)
+def create_tables():
+    SQLModel.metadata.create_all(engine)
 
-todo1 : Todo = Todo(content="first task")
-todo2 : Todo = Todo(content="second task")
+# todo1 : Todo = Todo(content="first task")
+# todo2 : Todo = Todo(content="second task")
 
 # session: seperate session for each transaction/functionality through engine
 session = Session(engine)
 
-# create todos in db
-session.add(todo1)
-session.add(todo2)
-print(f'Before commit {todo1}')
-session.commit()
-session.refresh(todo1)
-print(f'After commit {todo1}')
-session.close()
+# # create todos in db
+# session.add(todo1)
+# session.add(todo2)
+# print(f'Before commit {todo1}')
+# session.commit()
+# session.refresh(todo1)
+# print(f'After commit {todo1}')
+# session.close()
 
-app: FastAPI = FastAPI()
+# a better way is to make function of session
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# contextmanager will create tables firstly as app starts
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("Create Tables")
+    create_tables()
+    print("Tables Created")
+    yield
+
+
+app: FastAPI = FastAPI(lifespan=lifespan, title="Daily todos app", version='1.0.0')
 
 @app.get('/')
 async def root():
     return {"message": "Welcome to Daily TODOs App"}
 
 
-@app.post('/todos/')
-async def create_todo():
-    ...
+@app.post('/todos/', response_model=Todo)
+async def create_todo(todo:Todo, session:Annotated[Session, Depends(get_session)]):
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
 
-@app.get('/todos/')
-async def get_all():
-    ...
+@app.get('/todos/', response_model= list[Todo])
+async def get_all(session:Annotated[Session,Depends(get_session)]):
+    statement = select(Todo)
+    todos = session.exec(statement).all() 
+    return todos
 
-@app.get('/todos/{id}')
-async def get_single_todo():
-    ...
+@app.get('/todos/{id}', response_model=Todo)
+async def get_single_todo(id:int, todo:Todo, session:Annotated[Session,Depends(get_session)]):
+    todos = session.exec(select(Todo).where(Todo.id == id)).first()
+    return todos
 
 @app.put('/todos/{id}')
-async def update_todo():
-    ...
+async def update_todo(id:int,todo:Todo, session:Annotated[Session,Depends(get_session)]):
+    existing_todo = session.exec(select(Todo).where(Todo.id==id)).first()
+    if existing_todo:
+        existing_todo.content = Todo.content
+        existing_todo.is_completed = Todo.is_completed 
+        session.add(existing_todo)
+        session.commit()
+        session.refresh(existing_todo)
+        return existing_todo
+    else:
+        raise HTTPException (status_code=404, detail="No task found")  
 
 @app.delete('/todos/{id}')
-async def delete_todo():
-    ...        
-
+async def delete_todo(id:int, session:Annotated[Session,Depends(get_session)]):
+    todo = session.exec(select(Todo).where(Todo.id==id)).first()
+    session.delete(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
