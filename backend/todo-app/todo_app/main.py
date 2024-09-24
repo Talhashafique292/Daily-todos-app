4c52439a9d43b28719aaf438fe2a4c046813c6c0
Talha Shafique <talhashafique292@gmail.com>
2024-09-24 14:08:04 +0500

    engine and session created

--- backend/todo-app/todo_app/main.py
+++ backend/todo-app/todo_app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine, Session
 from todo_app import setting
 
 
@@ -12,11 +12,29 @@ class Todo(SQLModel, table = True):
     is_completed : bool = Field(default=False)
 
 # Connection string for engine
-connection_string = ""    
+connection_string : str = str(setting.DATABASE_URL).replace("postgresql","postgresql+psycopg") 
 
-# Engine:connection to db
-engine = create_engine(connection_string)
+# Engine:connection to db and is one for whole application
 
+engine = create_engine(connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
+
+# create tables
+SQLModel.metadata.create_all(engine)
+
+todo1 : Todo = Todo(content="first task")
+todo2 : Todo = Todo(content="second task")
+
+# session: seperate session for each transaction/functionality through engine
+session = Session(engine)
+
+# create todos in db
+session.add(todo1)
+session.add(todo2)
+print(f'Before commit {todo1}')
+session.commit()
+session.refresh(todo1)
+print(f'After commit {todo1}')
+session.close()
 
 app: FastAPI = FastAPI()
 
@@ -25,6 +43,23 @@ async def root():
     return {"message": "Welcome to Daily TODOs App"}
 
 
+@app.post('/todos/')
+async def create_todo():
+    ...
+
 @app.get('/todos/')
-async def read_todos():
-    return {"content": "dummy todo"}
+async def get_all():
+    ...
+
+@app.get('/todos/{id}')
+async def get_single_todo():
+    ...
+
+@app.put('/todos/{id}')
+async def update_todo():
+    ...
+
+@app.delete('/todos/{id}')
+async def delete_todo():
+    ...        
+
